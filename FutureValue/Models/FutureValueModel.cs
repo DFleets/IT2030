d7cf9bd239e23941ac46949a94b1710d48b9185b
Ravend20 <Fleetsd@yahoo.com>
2022-01-29 03:41:21 -0500

    Add project files.

---
+++ FutureValue/Models/FutureValueModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FutureValue.Models
+{
+    public class FutureValueModel
+    {
+        [Required(ErrorMessage = "Please enter your monthly investment amount.")]
+        [Range(1, 500, ErrorMessage = " The monthly investment amount must be between 1 and 500.")]
+        public decimal? MonthlyInvestment { get; set; }
+
+        [Required(ErrorMessage = "Please enter your yearly interest rate.")]
+        [Range(0.1, 10.0, ErrorMessage = "The yearly interest rate must be between 0.1 and 10.0.")]
+        public decimal? YearlyInterestRate { get; set; }
+
+        [Required(ErrorMessage = "Please enter the number of years.")]
+        [Range(1, 50, ErrorMessage = "The number of years must be between 1 and 50.")]
+        public int? Years { get; set; }
+        public decimal? CalculateFutureValue()
+        {
+            int? months = Years * 12;
+            decimal? monthlyInterestRate = YearlyInterestRate / 12 / 100;
+            decimal? futureValue = 0;
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+            return futureValue;
+
+        }
+    }
+}
